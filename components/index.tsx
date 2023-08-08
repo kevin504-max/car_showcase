3408b2c512a489f80241234b12cc52f0590a6cd6
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-08-08 18:49:26 -0300

    feat: layouts

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,11 @@
 import Hero from './Hero';
 import CustomButton from './CustomButton';
+import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
 
 export {
     Hero,
     CustomButton,
+    Navbar,
+    Footer,
 }
