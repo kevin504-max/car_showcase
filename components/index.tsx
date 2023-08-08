374df3cc7ec15981b5ab6b254323068c7e3a738d
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-08-08 19:50:39 -0300

    feat: cars catalogue

--- components/index.tsx
+++ components/index.tsx
@@ -2,10 +2,16 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './layouts/Navbar';
 import Footer from './layouts/Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
 
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
 }
