18fd95a0a3ee23e8668809e3e648a8fc8749ef21
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-08-09 17:42:50 -0300

    feat: show more functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from '@/constants';
 
@@ -40,6 +40,11 @@ export default async function Home({ searchParams }) {
                   <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore 
+              pageNumber={(searchParams.limit || 10) / 10 }
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
                 <h1 className="hero__title">
-                    Finde, book, or rent a car -- quickly and easily!
+                    Find, book, or rent a car -- quickly and easily!
                 </h1>
 
                 <p className="hero__subtitle">
